105506e4867aa8a08271f206e2e3a21bdf8313f6
IlyaChistov <gaplis322@gmail.com>
2022-11-29 21:33:12 +0300

    Changed

--- Homework/Homework_9/002/main.py
+++ Homework/Homework_9/002/main.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
-app = ApplicationBuilder().token("5889492635:AAEx2aL8Sa_K0JaOOK0N9UMXcKCW59mEqUg").build()
+app = ApplicationBuilder().token("").build()
 
 app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CommandHandler("calc", calc_command))
